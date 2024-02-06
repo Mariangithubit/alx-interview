1df573a8e6286e878f7f15c8d0120ba589c977d7
Mariangithubit <125896481+Mariangithubit@users.noreply.github.com>
2024-02-06 21:05:44 +0200

    Update 0-nqueens.py

--- 0x05-nqueens/0-nqueens.py
+++ 0x05-nqueens/0-nqueens.py
@@ -1,37 +1,44 @@
 #!/usr/bin/python3
-"""
-N Queens module
-"""
-
+""" N queens """
 import sys
 
 
-if __name__ == "__main__":
-    if (len(sys.argv) != 2):
-        print("Usage: nqueens N")
-        exit(1)
-    N = sys.argv[1]
-    if (not N.isdigit()):
-        print("N must be a number")
-        exit(1)
-    N = int(N)
-    if (N < 4):
-        print("N must be at least 4")
-        exit(1)
+if len(sys.argv) > 2 or len(sys.argv) < 2:
+    print("Usage: nqueens N")
+    exit(1)
 
-    def queens(n, i, a, b, c):
-        """
-        Translation of Niklaus Wirth's solution into Python
-        source: https://en.wikipedia.org/wiki/Eight_queens_puzzle
-        """
-        if i < n:
-            for j in range(n):
-                if j not in a and i+j not in b and i-j not in c:
-                    yield from queens(n, i+1, a+[j], b+[i+j], c+[i-j])
-        else:
-            yield a
+if not sys.argv[1].isdigit():
+    print("N must be a number")
+    exit(1)
 
-    for solution in queens(N, 0, [], [], []):
-        for idx, y in enumerate(solution):
-            solution[idx] = [idx, y]
-        print(solution)
+if int(sys.argv[1]) < 4:
+    print("N must be at least 4")
+    exit(1)
+
+n = int(sys.argv[1])
+
+
+def queens(n, i=0, a=[], b=[], c=[]):
+    """ find possible positions """
+    if i < n:
+        for j in range(n):
+            if j not in a and i + j not in b and i - j not in c:
+                yield from queens(n, i + 1, a + [j], b + [i + j], c + [i - j])
+    else:
+        yield a
+
+
+def solve(n):
+    """ solve """
+    k = []
+    i = 0
+    for solution in queens(n, 0):
+        for s in solution:
+            k.append([i, s])
+            i += 1
+        print(k)
+        k = []
+        i = 0
+
+
+solve(n)
